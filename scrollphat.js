39153604e615d4653ee0e70d512bf6a9303af155
brian-burton <brian-burton@users.noreply.github.com>
2017-10-19 22:50:23 +0000

    Linted JS; new Clear node; updated README

--- scrollphat.js
+++ scrollphat.js
@@ -1,37 +1,37 @@
 // http://www.issi.com/WW/pdf/31FL3730.pdf for LED driver chip spec
 
-module.exports = function(RED) {
-  
-  function spSetPixelNode(config) {
-  RED.nodes.createNode(this,config);
-    var node = this;
-    var i2c = require('i2c-bus');
-    //Bus address of the I2C bus
-    var BUS_ADDRESS = 1;
-    // I2C Address of the ScrollPhat
-    var SP_ADDRESS = 0x60;
-    // Starting address for the matrix 1 data register
-    var SP_COMMAND = 0x01;
-    //buffer for pixel data which will be written the matrix 1 data register
-    var buffer = new Uint8Array(12).fill(0x00);
-    //After the 11 columns, we need to send 0xFF to finish the message to the I2C slave
-    buffer[11] = 0xff;
+module.exports = function (RED) {
 
-    try{
+  var i2c = require("i2c-bus");
+  //Bus address of the I2C bus
+  var BUS_ADDRESS = 1;
+  // I2C Address of the ScrollPhat
+  var SP_ADDRESS = 0x60;
+  // Starting address for the matrix 1 data register
+  var SP_COMMAND = 0x01;
+  //buffer for pixel data which will be written the matrix 1 data register
+  var buffer = new Uint8Array(12).fill(0x00);
+  //After the 11 columns, we need to send 0xFF to finish the message to the I2C slave
+  buffer[11] = 0xff;
+
+  function spSetPixelNode(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+
+    try {
       var scrollphat = i2c.openSync(BUS_ADDRESS);
       if (scrollphat.hasOwnProperty("_forceAccess")) {
         node.status({fill: "green", shape: "dot", text: "connected"});
-      }
-      else {
+      } else
+      {
         node.status({fill: "red", shape: "ring", text: "error"});
         throw "I2C connection error";
       }
-    }
-    catch (e) {
+    } catch (e) {
       node.error("There was a problem: " + e);
     }
 
-    node.on('input', function(msg) {
+    node.on("input", function(msg) {
       // Preflight check on msg.payload - must have a row, a column and a value (true/false for on/off)
       if (typeof msg.payload.x === "number" && typeof msg.payload.y === "number" && typeof msg.payload.value === "boolean") {
         if ( 0 > msg.payload.x || msg.payload.x > 10) { node.error("Column is out of bounds", msg); }
@@ -42,29 +42,71 @@ module.exports = function(RED) {
         node.ledValue = msg.payload.value || false;
         node.additive = msg.payload.additive && true;
 
-        //Add LEDs to existing pattern by default or reset to jsut new pixel if user specifically sets msg.payload.additive to false
-        if (node.additive == false){ buffer.fill(0x00,0,11); }
+        //Add LEDs to existing pattern by default or reset to just new pixel if user specifically sets msg.payload.additive to false
+        if (node.additive === false){ buffer.fill(0x00,0,11); }
 
         //This is transcribed from set_pixel in https://github.com/pimoroni/scroll-phat/blob/master/library/scrollphat/IS31FL3730.py
         //Lots of bitwise shifting to set the right pixel in column x ( buffer elements 0-10) and row y (bits 0-4).
-        if (node.ledValue) { buffer[node.xPos] |= (1 << node.yPos); }
-        else { buffer[node.xPos] &= ~(1 << node.yPos); }
+        if (node.ledValue) { buffer[node.xPos] |= (1 << node.yPos); } else { buffer[node.xPos] &= ~(1 << node.yPos); }
       }
 
       //Write the entire buffer to the Scroll Phat
-      scrollphat.writeI2cBlockSync(0x60, 0x01, 12, buffer);
+      scrollphat.writeI2cBlockSync(SP_ADDRESS, SP_COMMAND, 12, buffer);
     });
-    node.on('close', function() {
+    node.on("close", function() {
       try {
         scrollphat.closeSync();
-        if (scrollphat._peripherals.length == 0) { node.warn("Scroll Phat i2c connection closed."); }
-        else {throw "Failed to close scrollphat connection";}
-      }
-      catch (e) {
+        if (scrollphat._peripherals.length === 0) {
+          //node.warn("Scroll Phat i2c connection closed.");
+        } else {throw "Failed to close scrollphat connection";}
+      } catch (e) {
         node.error("Error: " + e);
       }
     });
   }
 
-  RED.nodes.registerType("spSetPixel",spSetPixelNode);
-}
+  function spClearNode(config) {
+    RED.nodes.createNode(this,config);
+    var node = this;
+
+    try {
+      var scrollphat = i2c.openSync(BUS_ADDRESS);
+      if (scrollphat.hasOwnProperty("_forceAccess")) {
+        node.status({fill: "green", shape: "dot", text: "connected"});
+      } else {
+        node.status({fill: "red", shape: "ring", text: "error"});
+        throw "I2C connection error";
+      }
+    } catch (e) {
+      node.error("There was a problem: " + e);
+    }
+
+    node.on("input", function(msg) {
+      // Preflight check on msg.payload
+      if (msg.payload === true || msg.payload.toString().toLowerCase() === "on" || msg.payload === 1) {
+        buffer.fill(0x1F,0,11);
+      } else if (msg.payload === false || msg.payload.toString().toLowerCase() === "off" || msg.payload === 0) {
+        buffer.fill(0x00,0,11);
+      } else {
+        node.warn("Invalid Scroll pHAT clear msg.payload");
+      }
+
+      //Write the entire buffer to the Scroll Phat
+      scrollphat.writeI2cBlockSync(SP_ADDRESS, SP_COMMAND, 12, buffer);
+    });
+    node.on("close", function() {
+      try {
+        scrollphat.closeSync();
+        if (scrollphat._peripherals.length === 0) {
+          // node.warn("Scroll Phat i2c connection closed.");
+        } else {throw "Failed to close scrollphat connection";}
+      } catch (e) {
+        node.error("Error: " + e);
+      }
+    });
+  }
+
+  RED.nodes.registerType("spSetPixel", spSetPixelNode);
+  RED.nodes.registerType("spClear", spClearNode);
+};
+
